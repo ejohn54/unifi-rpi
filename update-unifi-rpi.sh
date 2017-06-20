fde6d34f3d9c6e34a5954eaeb2670f374960aafb
ejohn54 <ejohnson@datasci.com>
2017-06-20 15:33:17 -0500

    Update update-unifi-rpi.sh

--- update-unifi-rpi.sh
+++ update-unifi-rpi.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-# To update the UniFi Controller software to the latest version, simply use the standard Debian package update and upgrade commands:
-sudo apt-get update && sudo apt-get upgrade -y
+# Update the package list, update all packages and remove any packages that are no longer required
+sudo apt-get update -y && sudo apt-get dist-upgrade -y && sudo apt-get autoremove -y
 
 #Ensure the latest Raspberry Pi firmware has been applied, using the following commands:
-sudo apt-get install rpi-update && echo Y | sudo rpi-update
+sudo apt-get install rpi-update
+sudo rpi-update
 
 
 # Alternatively, if you just want to update the UniFi Controller software only, run the initial package installation command again:
