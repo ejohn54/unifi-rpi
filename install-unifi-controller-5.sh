471a7039de51f405ebdf19428a9496cb8fc3f589
ejohn54 <ejohnson@datasci.com>
2017-07-24 14:21:26 -0500

    Update install-unifi-controller-5.sh

--- install-unifi-controller-5.sh
+++ install-unifi-controller-5.sh
@@ -6,7 +6,6 @@ sudo insserv avahi-daemon
 
 
 # Add the UniFi repository to the sources list, using the following commands:
-sudo rm -f /etc/apt/sources.list.d/ubnt.list
 echo 'deb http://www.ubnt.com/downloads/unifi/debian stable ubiquiti' | sudo tee /etc/apt/sources.list.d/100-ubnt.list > /dev/null
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 06E85760C0A52C50
 sudo apt-get update
@@ -21,7 +20,7 @@ sudo apt-get install unifi -y
 # Update to the Snappy Java Library, using the following commands:
 sudo rm /usr/lib/unifi/lib/snappy-java-1.0.5.jar
 sudo wget -O /usr/lib/unifi/lib https://repo1.maven.org/maven2/org/xerial/snappy/snappy-java/1.1.4-M3/snappy-java-1.1.4-M3.jar
-sudo ln -s /usr/lib/unifi/lib/snappy-java-1.1.2.6.jar /usr/lib/unifi/lib/snappy-java-1.0.5.jar
+sudo ln -s /usr/lib/unifi/lib/snappy-java-1.1.4-M3.jar /usr/lib/unifi/lib/snappy-java-1.0.5.jar
 
 # update to Oracle Java 8 by performing the following steps
 sudo apt-get install oracle-java8-jdk -y
