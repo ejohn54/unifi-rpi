73a67fbb0714c9f0299f00040ea8c1084cbff3bb
ejohn54 <ejohnson@datasci.com>
2017-07-25 09:33:49 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-# Update the package list and update all packages 
-sudo apt-get update && sudo apt-get upgrade -y 
+# Update the package list and update all packages
+sudo apt-get update
+yes | sudo apt-get upgrade --show-upgraded --fix-broken -y 
 
 # Ensure the latest Raspberry Pi firmware has been applied
 sudo rpi-update 
 
 # Remove any packages that are no longer required
-sudo apt-get autoremove 
+sudo apt-get autoremove -y
 sudo apt-get autoclean 
 sudo reboot
