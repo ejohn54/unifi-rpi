45ad5a1d06bbbbf59cbb691f71e7b1dde4c71c11
Eric Johnson <ejohnson@datasci.com>
2017-07-26 17:09:13 -0500

    Added Avahi configuration script

---
+++ configure-avahi.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Create the multiple.service file to respond to the .local name
+sudo tee /etc/avahi/services/multiple.service <<EOF
+<?xml version="1.0" standalone='no'?>
+<!DOCTYPE service-group SYSTEM "avahi-service.dtd">
+<service-group>
+        <name replace-wildcards="yes">%h</name>
+        <service>
+                <type>_device-info._tcp</type>
+                <port>0</port>
+                <txt-record>model=RackMac</txt-record>
+        </service>
+        <service>
+                <type>_ssh._tcp</type>
+                <port>22</port>
+        </service>
+</service-group>
+EOF
+
+sudo /etc/init.d/avahi-daemon restart
+
