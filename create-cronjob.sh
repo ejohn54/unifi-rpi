5d8421b500368310ce4b8576f30dcfcf9b85c59c
ejohn54 <ejohnson@datasci.com>
2018-04-12 11:08:25 -0500

    Update create-cronjob.sh

--- create-cronjob.sh
+++ create-cronjob.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+# Make sure logs folder is created.
+mkdir -p /home/pi/logs
+
 crontab -l > mycron
 #echo new cron into cron file
-echo "30 02 * * * bash /home/pi/update.sh >> /home/pi/logs/cronlog" >> mycron
+echo "00 01 * * SUN bash /home/pi/unifi-rpi/get-latest.sh >> /home/pi/logs/gitpull.log" >> mycron
+echo "30 02 * * SUN bash /home/pi/update.sh >> /home/pi/logs/cronlog" >> mycron
 #install new cron file
 crontab mycron
 rm mycron
