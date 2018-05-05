2f249fa2563d72a2b6e61ec5caf711193a6909d8
ejohn54 <ejohnson@datasci.com>
2018-05-05 10:21:28 -0500

    Update create-cronjob.sh

--- create-cronjob.sh
+++ create-cronjob.sh
@@ -5,8 +5,8 @@ mkdir -p /home/pi/logs
 
 crontab -l > mycron
 #echo new cron into cron file
-echo "00 01 * * SUN bash /home/pi/unifi-rpi/get-latest.sh >> /home/pi/logs/gitpull.log" >> mycron
-echo "30 02 * * SUN bash /home/pi/update.sh >> /home/pi/logs/update_sh.log" >> mycron
+echo "00 01 * * SUN bash /home/pi/unifi-rpi/get-latest.sh >> /home/pi/logs/gitpull.log 2>&1" >> mycron
+echo "30 02 * * SUN bash /home/pi/update.sh >> /home/pi/logs/update_sh.log 2>&1" >> mycron
 #install new cron file
 crontab mycron
 rm mycron
